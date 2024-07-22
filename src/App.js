b5122aa45a2146b78c2c12edde1f9034ad14edb5
Benbouquet <bouquetbenjamin@gmail.com>
2024-07-22 12:37:52 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RoyaltyCalculator from './RoyaltyCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RoyaltyCalculator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
